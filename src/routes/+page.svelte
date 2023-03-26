9a8d385d092de5e1fd78d125dc049f150b1b7211
hamzaR <hamza.rebb@gmail.com>
2023-03-26 17:40:12 +0200

    start to create the route about my projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,5 @@
 <script>
-    import { onMount } from 'svelte';
     import Welcome from './welcome.svelte';
-    import Index from './index.svelte';
-
-    let projects = [];
-
-    onMount(async () => {
-        const response = await fetch('src/routes/projects.json');
-        projects = await response.json();
-    });
 </script>
 
 <Welcome />
-<Index projects={projects}/>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { onMount } from 'svelte';
+    import Index from './index.svelte';
+
+    let projects = [];
+
+    onMount(async () => {
+        const response = await fetch('src/routes/projects.json');
+        projects = await response.json();
+    });
+</script>
+
+<Index projects={projects}/>
