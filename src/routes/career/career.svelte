42b3fb4ae7778eee612d7d5da1389ee5199d8fa2
hamzaR <hamza.rebb@gmail.com>
2023-03-26 18:43:47 +0200

    start to dev the career timeline

---
+++ src/routes/career/career.svelte
@@ -0,0 +1,9 @@
+<style>
+    .title {
+        background: linear-gradient(90deg, rgba(49,183,231,1) 11%, rgba(0,119,192,1) 92%);
+        color: white;
+        font-size: revert-layer;
+        text-align: center;
+        font-family: initial;
+    }
+</style>
