693a7ec4a49dc8fd5876362fd2fc3e05db70a2d1
hamzaR <hamza.rebb@gmail.com>
2023-03-26 18:49:30 +0200

    start to prepare the social network componants and prepare to work about the home page

--- src/routes/welcome.svelte
+++ src/routes/welcome.svelte
@@ -2,6 +2,10 @@
     Hi there, welcome to my Personal Profile
 </h1>
 
+<nav>
+    <p>Here you can find all sections in my Profile</p>
+</nav>
+
 <style>
     .title {
         background: linear-gradient(90deg, rgba(49,183,231,1) 11%, rgba(0,119,192,1) 92%);
