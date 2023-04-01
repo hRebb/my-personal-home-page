7ca7c9e572b095238d322a325dc791ea0184d0c4
hamzaR <hamza.rebb@gmail.com>
2023-04-01 16:18:06 +0200

    start to dev the carousel

--- src/routes/stack/+page.svelte
+++ src/routes/stack/+page.svelte
@@ -1,13 +1,54 @@
 <script>
+    import {onMount} from "svelte";
     import Carousel from "./carousel.svelte";
 
     const images = [
-        'src/routes/stack/img/bootstrap.png',
+        'src/routes/stack/img/bootstrap-logo.png',
         'src/routes/stack/img/css-3.png',
         'src/routes/stack/img/git.png',
         'src/routes/stack/img/html.png',
         'src/routes/stack/img/icons8-node-js-48.png',
     ];
+
+    let currentIndex = 0;
+
+    function onNext() {
+        currentIndex = (currentIndex + 1) % images.length;
+    }
+
+    function onPrev() {
+        currentIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
+    }
+
+    onMount(() => {
+        const intervalId = setInterval(() => {
+            onNext();
+        }, 3000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    });
 </script>
 
-<Carousel {images} />
+<div class="container">
+    <Carousel images={images} currentIndex={currentIndex} />
+    <div class="buttons">
+      <button on:click={onPrev}>Previous</button>
+      <button on:click={onNext}>Next</button>
+    </div>
+  </div>
+  
+  <style>
+    .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  
+    .buttons {
+      display: flex;
+      justify-content: center;
+      margin-top: 1rem;
+    }
+  </style>
--- src/routes/stack/carousel.svelte
+++ src/routes/stack/carousel.svelte
@@ -1,49 +1,16 @@
 <script lang="ts">
-    import {onMount} from 'svelte';
-    import {onCleanup} from 'svelte';
-    import {onInterval} from 'svelte';
-    import {setContext} from 'svelte';
-    import {getContext} from 'svelte';
-
-    const INTERVAL_DELAY = 3000;
-
-    interface Props {
-        images: string[];
-    }
-
-    type Context = number;
-
-    export let images: Props["images"] = [];
-
-    const [current, setCurrent] = setContext<Context>('current', 0);
-
-    let interval: () => void;
-
-    onMount(() => {
-        interval = onInterval(() => {
-            setCurrent((contextImages) => (contextImages + 1) % images.length)
-        }, INTERVAL_DELAY); 
-    });
-
-    onCleanup(() => {
-        interval();
-    });
-
-    function next() {
-        setCurrent((contextImages) => (contextImages + 1) % images.length)
-    }
-
-    function previous() {
-        setCurrent((contextImages) => (contextImages - 1) % images.length)
-    }
+    export let images: string[];
+    export let currentIndex: number;
 </script>
 
 <div class="carousel">
-    <button on:click={previous}>Previous</button>
-    <img src={images[current]} alt="carousel image">
-    <button on:click={next}>Next</button>
+    <div class="slides" style="transform: translateX(-${currentIndex * 100}%)">
+        {#each images as image }
+            <img src={image} alt="" />
+        {/each}
+    </div>
 </div>
 
 <style>
-    @import "./carousel.css";
+    @import './carousel.css';
 </style>
