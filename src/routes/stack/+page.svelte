cc73317d11727db3c1f1dbb02d603dd812150143
hamza_rebb <hamza.rebb@gmail.com>
2023-06-09 19:15:00 +0200

    devops stack

--- src/routes/stack/+page.svelte
+++ src/routes/stack/+page.svelte
@@ -1,17 +1,24 @@
 <script>
     import Front from "./front/front.svelte";
     import Back from "./back/back.svelte";
+    import Devops from "./devops/devops.svelte";
 </script>
 
 <h1 class="title">
     My Stack
   </h1>
 
-<div class="container">
+<div class="container frontend">
     <Front/>
 </div>
 <br>
-<div class="container">
+
+<div class="container backend">
     <Back />
+</div>
+<br>
+
+<div class="container devops">
+    <Devops />
 </div>
 <br>
---
+++ src/routes/stack/devops/devops.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import type { FetchResponse } from"node-fetch";
+
+    let stackTechnique: string[] = [];
+
+    onMount(async() => {
+        try{
+            const response: FetchResponse = await fetch('src/routes/stack/devops/devops.json');
+            const data: {stackTechnique: string[]} = await response.json();
+            stackTechnique = data.stackTechnique;
+        } catch (error) {
+            console.error('Erreur lors de la récupération des données JSON:', error);
+        }
+    })
+</script>
+
+<table>
+    <thead>
+        <th>
+            DevOps Tools
+        </th>
+        <tr>
+            <th>Title</th>
+            <th>Badge</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each stackTechnique as item }
+            <tr>
+                <td>
+                    {item}
+                </td>
+                <td>
+                    {#if item === 'Docker'}
+                        <img src="https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white" alt="Docker">
+                    {:else if item === 'Vagrant'}
+                        <img src="https://img.shields.io/badge/vagrant-%231563FF.svg?style=for-the-badge&logo=vagrant&logoColor=white" alt="Vagrant">
+                    {:else if item === 'GitHub Actions'}
+                        <img src="https://img.shields.io/badge/github%20actions-%232671E5.svg?style=for-the-badge&logo=githubactions&logoColor=white" alt="GitHub Actions">
+                    {:else if item === 'Virtualbox'}
+                        <img src="https://img.shields.io/badge/VirtualBox-183A61.svg?style=for-the-badge&logo=VirtualBox&logoColor=white" alt="Virtualbox">
+                    {:else if item === 'SonarQube'}
+                        <img src="https://img.shields.io/badge/SonarQube-4E9BCD.svg?style=for-the-badge&logo=SonarQube&logoColor=white" alt="SonarQube">
+                    {:else if item === 'SonarCloud'}
+                        <img src="https://img.shields.io/badge/SonarCloud-F3702A.svg?style=for-the-badge&logo=SonarCloud&logoColor=white" alt="SonarCloud">
+                    {/if}
+                </td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
